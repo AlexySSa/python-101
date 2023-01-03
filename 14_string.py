4f42f7187c3e93f572d261b56428bf372c880e1e
Alexy <aassuriano@gmail.com>
2023-01-03 17:50:12 +0000

    String recargado

---
+++ 14_string.py
@@ -0,0 +1,16 @@
+text = "ella sabe programar en python"
+
+'''
+
+print("javaScript" in text)
+print("python" in text)
+
+if "python" in text:
+    print("Python is a good programming language")
+else:
+    print("tambien eligiste bien")
+
+'''
+
+size = len("amor")
+print(size)
