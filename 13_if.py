7eb11ee44be89f0f4fa30ef0c61368ed96ad7919
Alexy <aassuriano@gmail.com>
2023-01-03 01:54:38 +0000

    else if finally

---
+++ 13_if.py
@@ -0,0 +1,30 @@
+if True: 
+  print("deberia ejecutarse ")
+
+  if False:
+    print("no deberia ejecutarse ")
+
+pet = input("cual es tu mascota favorita => ")
+if pet == "perro":
+  print("el perro es el mas favorito")
+
+elif pet == "gato":
+  print("espero que tengas suerte")
+
+elif pet == "pez":
+  print("que no se te ahogue crack")
+
+else:
+  print("que tines un peje lagarto o que ")
+  
+'''
+
+stock = int(input("digita el stock =>" + " "))
+
+if stock >= 100 and stock <= 1000:
+  print("el stock es correcto por que es de " + str(stock))
+else:
+  print("el stock no es correcto por que es de " + str(stock))
+  
+'''
+
