4631e0034eb0a7e57984a541db4b1a0694eda10b
Alexy <aassuriano@gmail.com>
2023-01-04 03:53:33 +0000

    listas

---
+++ 16_lists.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(numbers)
+print(type(numbers))
+
+tasks = ["make a dishes", "play videogame"]
+print(tasks)
+
+types = [1, True, "hola papu"]
+print(types)
+
+print(numbers[1])
+print(tasks[0])
+
+tasks[0] = "se puede cambiar :O"
+print(tasks)
+
+tasks[0] = "do the dishes"
+print(tasks)
+
+print(numbers[:3])
+print(True in types)
+print("hola papu" in types)
