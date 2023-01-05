4c92f372b76036da4b6b9a24952628033410da5f
Alexy <aassuriano@gmail.com>
2023-01-05 03:41:55 +0000

    piedra papel o tijeras pero ahora mejor

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import random
+
+opcions = ("piedra", "papel", "tijeras")
+
 user_opcion = input("piedra, papel o tijeras => ")
 user_opcion = user_opcion.lower()
-PC_opcion = ("papel")
-print(PC_opcion)
+
+if not user_opcion in opcions:
+  print("la opcion que eligiste nada que ver por que es ", user_opcion)
+  exit()
+  
+PC_opcion = (random.choice(opcions))
+
+print("user opcion => ", user_opcion)
+print("PC opcion => ",PC_opcion)
 
 if(user_opcion == PC_opcion):
   print("este es un empate")
@@ -12,19 +23,19 @@ elif user_opcion == "piedra":
     print("user gano")
   else:
    print("papel gana a piedra")
-   print("pc gano")
+   print("PC gano")
 elif user_opcion == "papel":
   if PC_opcion == "piedra":
     print("papel gana a piedra")
     print("user gano")
   else:
     print("tijeras gana a papel ")
-    print("pc gano")
+    print("PC gano")
 elif user_opcion == "tijeras":
   if PC_opcion == "papel":
     print("tijeras gana a papel")
     print("user gano")
   else:
     print("piedra gana a tijeras")
-    print("pc gano")
+    print("PC gano")
   
